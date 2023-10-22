5e5ac1ae7c1ebca1b20d4d504884e2e260c830af
Aryeh Rotberg <aryeh.rotberg@gmail.com>
2023-10-22 22:14:27 +0300

    Updated streamlit app

--- app.py
+++ app.py
@@ -9,14 +9,18 @@ model = YOLO('runs/segment/train/weights/best.pt')
 st.title('Mobile Phone Defect Segmentation')
 file = st.file_uploader('Upload image')
 
-predict_btn = st.button('Predict')
+predict_btn = st.button('Detect')
 
 if predict_btn and file:
     image = Image.open(file)
     results = model.predict(image, save=False, verbose=False)
 
-    st.image(results[0].plot())
-
     num_scratches = results[0].boxes.cls.tolist().count(0)
 
-    st.write(f'Detected {num_scratches} scratches.')
+    if num_scratches == 0:
+        st.info("No scratches detected! You're good to go.")
+    
+    else:
+        st.error(f'⚠️ Detected {num_scratches} scratches.')
+
+    st.image(results[0].plot())
