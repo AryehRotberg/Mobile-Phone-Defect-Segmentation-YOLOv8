f89582f71954a22bda910f20d8228b20bd7c9ba4
Aryeh Rotberg <aryeh.rotberg@gmail.com>
2023-10-31 22:29:22 +0200

    Removed model_evaluation file and uncommented data ingestion pipeline

--- src/pipelines/pipeline.py
+++ src/pipelines/pipeline.py
@@ -2,7 +2,6 @@ import logging
 
 from src.components.data_ingestion import DataIngestion
 from src.components.model_trainer import ModelTrainer
-from src.components.model_evaluation import ModelEvaluation
 
 
 if __name__ == '__main__':
@@ -12,17 +11,14 @@ if __name__ == '__main__':
                         level=logging.INFO)
 
     # Data Ingestion
-    # data_ingestion = DataIngestion()
-    # data_ingestion.download_roboflow_project()
-    # data_ingestion.extract_zip_file('Mobile Phone Defect Segmentation.v1i.yolov8.zip')
+    data_ingestion = DataIngestion()
+    data_ingestion.download_roboflow_project()
+    data_ingestion.extract_zip_file('Mobile Phone Defect Segmentation.v1i.yolov8.zip')
 
-    # logging.info('Downloaded roboflow data and updated data directory in config.\n')
+    logging.info('Downloaded roboflow data and updated data directory in config.\n')
 
     # Model Training
     model_trainer = ModelTrainer()
     study = model_trainer.create_optuna_pipeline(n_trials=7)
 
     logging.info(f'Tuned YOLO model. Best Results -> {model_trainer.get_best_study_results(study)}')
-
-    # model_evaluation = ModelEvaluation()
-    # logging.info(f'Model Parameters -> \n{model_evaluation.get_parameters()}')
